9a641a489f6cab3f178229895c5e2de0c8aacbd9
kuelteabmas <fallmuhamed@gmail.com>
2023-03-10 23:43:46 -0500

    Initialize mealtracker microservice for HealthManager

---
+++ mealtracker/src/main/java/com/totaldevservices/mealtracker/MealtrackerApplication.java
@@ -0,0 +1,11 @@
+package com.totaldevservices.mealtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealtrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MealtrackerApplication.class, args);
+    }
+}

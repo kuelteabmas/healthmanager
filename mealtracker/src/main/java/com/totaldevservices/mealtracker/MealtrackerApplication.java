2c9cea8c4e6918beb0d66004396e19c7c271fe2c
kuelteabmas <fallmuhamed@gmail.com>
2023-03-22 22:49:27 -0400

    Implemented Eureka Clients
    - Added client dependencies
    - Added configuration in each microservice

--- mealtracker/src/main/java/com/totaldevservices/mealtracker/MealtrackerApplication.java
+++ mealtracker/src/main/java/com/totaldevservices/mealtracker/MealtrackerApplication.java
@@ -2,8 +2,10 @@ package com.totaldevservices.mealtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MealtrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MealtrackerApplication.class, args);

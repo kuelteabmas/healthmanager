4a05995ec6432e2aa4e4b3041bba82d6ada6f1dc
kuelteabmas <fallmuhamed@gmail.com>
2023-03-24 00:25:24 -0400

    Implemented Service layer for Water Intake Tracker micro service
    - Added Get all items service functionality

---
+++ water-intake-tracker/src/main/java/com/totaldevservices/waterintaketracker/service/WaterIntakeTrackerService.java
@@ -0,0 +1,10 @@
+package com.totaldevservices.waterintaketracker.service;
+
+import com.totaldevservices.waterintaketracker.dto.WaterIntakeTracker;
+
+import java.util.List;
+
+public interface WaterIntakeTrackerService {
+
+    List<WaterIntakeTracker> getAllWaterIntakeTrackerItems();
+}

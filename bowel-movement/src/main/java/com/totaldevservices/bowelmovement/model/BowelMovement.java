149d82ca2f5e6eee2ce00d30b680182de17496a7
kuelteabmas <fallmuhamed@gmail.com>
2023-03-27 17:29:59 -0400

    Refactored bowelmovemt with DTO Pattern

--- bowel-movement/src/main/java/com/totaldevservices/bowelmovement/model/BowelMovement.java
+++ bowel-movement/src/main/java/com/totaldevservices/bowelmovement/model/BowelMovement.java
@@ -1,4 +1,4 @@
-package com.totaldevservices.bowelmovement.dto;
+package com.totaldevservices.bowelmovement.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- bowel-movement/src/main/java/com/totaldevservices/bowelmovement/service/BowelMovementServiceImpl.java
+++ bowel-movement/src/main/java/com/totaldevservices/bowelmovement/service/BowelMovementServiceImpl.java
@@ -1,21 +1,28 @@
 package com.totaldevservices.bowelmovement.service;
 
 import com.totaldevservices.bowelmovement.BowelMovementRepository;
-import com.totaldevservices.bowelmovement.dto.BowelMovement;
+import com.totaldevservices.bowelmovement.dto.BowelMovementResponse;
+import com.totaldevservices.bowelmovement.mapper.BowelMovementMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class BowelMovementServiceImpl implements BowelMovementService {
 
     private BowelMovementRepository repository;
+    private BowelMovementMapper bowelMovementMapper;
 
     @Override
-    public List<BowelMovement> getAllBowelMovementJournalItems() {
-        List<BowelMovement> bmJournalItems = repository.findAll();
-        return bmJournalItems;
+    public List<BowelMovementResponse> getAllBowelMovementJournalItems() {
+        List<BowelMovementResponse> bmJournalItemsResponses = repository.findAll()
+                .stream()
+                .map(bowelMovementMapper)
+                .collect(Collectors.toList());
+
+        return bmJournalItemsResponses;
     }
 }

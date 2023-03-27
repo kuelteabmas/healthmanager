bcd3d068630e69be810b6bb4ca04629c655e0f96
kuelteabmas <fallmuhamed@gmail.com>
2023-03-27 18:56:45 -0400

    Refactored MealtrackerRequest in mealtracker microservice

--- mealtracker/src/main/java/com/totaldevservices/mealtracker/dto/MealTrackerRequest.java
+++ mealtracker/src/main/java/com/totaldevservices/mealtracker/dto/MealTrackerRequest.java
@@ -2,18 +2,10 @@ package com.totaldevservices.mealtracker.dto;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class MealTrackerRequest {
-
 //    private Meal meal;
     private String food;
-
-    private LocalDateTime localDateTimeOfMeal;
-
     private boolean feltIll;
-
     private String illSymptoms;
-
 }

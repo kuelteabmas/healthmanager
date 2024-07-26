b70a830445d6bd8f8524af3c78bfe16099a8aa29
kuelteabmas <fallmuhamed@gmail.com>
2024-07-25 20:00:40 -0400

    Update RabbitMQConfig.java
    Added SimpleRabbitListenerContainerFactory which creates a listener container from the connection factory

--- amqp/src/main/java/com/totaldevservices/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/totaldevservices/amqp/RabbitMQConfig.java
@@ -3,6 +3,7 @@ package com.totaldevservices.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -16,11 +17,21 @@ public class RabbitMQConfig {
 
     private final ConnectionFactory connectionFactory;
 
+    // Template to send message to queue
     @Bean
     public AmqpTemplate amqpTemplate() {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConverter());
         return rabbitTemplate;
+    }
+
+    // Listener to receive message in the queue
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory = new SimpleRabbitListenerContainerFactory();
+        rabbitListenerContainerFactory.setConnectionFactory(connectionFactory);
+        rabbitListenerContainerFactory.setMessageConverter(jacksonConverter());
+        return rabbitListenerContainerFactory;
     }
 
     @Bean
